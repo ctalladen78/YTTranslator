73b271b4b2e45e0cc623f18a2bed6371d8996377
CyTalladen <cyrus.talladen78@gmail.com>
2023-12-02 04:07:27 +0300

    init commit

--- app.py
+++ app.py
@@ -195,7 +195,8 @@ def get_chat_response(prompt,txt):
         You are a language expert assistant in Japanese, Chinese, Korean.
         If you are asked about unrelated expertise, then politely reply I don't know.
         You are expert in vocabulary, grammatical cases, conjugation, slang.
-        If there are grammatical errors, and provide a partial analysis, improvise and approximate.
+        If the sentence is incomplete or has grammatical mistakes, and provide a partial analysis.
+        Make an attempt at analysis.
         Analyse this sentence into its translation, pronunciation, 
         grammatical structures in terms of linguistic, phonetic meanings
         based on the following context:
